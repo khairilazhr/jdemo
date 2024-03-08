f239c3d2eaa6953b5ce81c76a0cd92ee303027f2
Khairil Azhar <khairilazhr@gmail.com>
2024-03-08 09:05:50 +0800

    jdemo

--- src/main/webapp/app/core/jhi-footer/jhi-footer.vue
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="footer" class="footer">
     <p v-text="$t('footer')">This is your footer</p>
+    <li>khairilazhr@gmail.com</li>
+    <li>011-10517869</li>
   </div>
 </template>
 
